f361a0b2b25eddf020f58c37726028b9e0cd3f1c
yasattar <yasmin.attar72@gmail.com>
2020-06-07 01:01:37 +0530

    init

---
+++ lib/components/icon_content.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../constant.dart';
+
+class IconContent extends StatelessWidget {
+//  const IconContent({
+//    Key key,
+//  }) : super(key: key);
+  IconContent({this.icon,this.label});
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment:MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(icon,size: 80.0,),
+        SizedBox(height: 15.0,),
+        Text(label,
+          style: kLabelTextStyle,)
+      ],);
+  }
+}
---
+++ lib/screens/results_page.dart
@@ -0,0 +1,54 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutterapp/constant.dart';
+import '../components/reusable_card.dart';
+import '../components/bottom_button.dart';
+class ResultPage extends StatelessWidget {
+  ResultPage({@required this.interpretation,@required this.bmiResult,@required this.resultText});
+
+  final String bmiResult;
+  final String resultText;
+  final String interpretation;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BMI CALCULATOR"),
+      ),
+        body:Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                child:Container(
+                  padding: EdgeInsets.all(15.0),
+                  child: Text('Your Result',style: kTitleTextStyle,),
+                )
+              ),
+              Expanded(
+                flex: 5,
+                  child: ReusableCard(colour: kActiveCardColor,
+                  cardChild: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(resultText,style: kResultTextStyle,),
+                      Text(bmiResult,style: kBMITextStyle,),
+                      Text(interpretation,
+                            textAlign: TextAlign.center,
+                            style: kBodyTextStyle,
+                      ),
+                    ],
+                    
+                  ),),
+              ),
+              BottomButton(buttonTitle: 'RE-CALCULATE',
+                onTap: (){
+                  Navigator.pop(context);
+                },
+              )
+            ],
+        ),
+    );
+  }
+}
